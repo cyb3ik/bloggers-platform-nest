0f4fc392ed992a2b33df78bdd5955a6e1f76c457
KBdev-PC <cyb3ik88@mail.ru>
2026-07-05 17:57:44 +0300

    FIXES

--- src/modules/bloggers-platform/likes/dto/like-view.dto.ts
+++ src/modules/bloggers-platform/likes/dto/like-view.dto.ts
@@ -1,12 +1,12 @@
 import { LikeDocument } from "../domain/like.entity"
 
 export class LikeViewDto {
-    addedAt: Date
+    addedAt: string
     userId: string
     login: string
 
     constructor(like: LikeDocument) {
-        this.addedAt = like.createdAt
+        this.addedAt = like.createdAt.toISOString()
         this.userId = like.userId.toString()
         this.login = like.userLogin
     }
