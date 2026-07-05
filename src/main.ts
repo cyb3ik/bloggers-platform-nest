2cb635b6bc38109a3494273621f1b4a77ac77d71
KBdev-PC <cyb3ik88@mail.ru>
2026-07-05 16:08:43 +0300

    render adjustments

--- src/main.ts
+++ src/main.ts
@@ -30,7 +30,8 @@ async function bootstrap() {
 
   app.useGlobalFilters(new HttpExceptionFilter())
 
-  await app.listen(process.env.PORT ?? 5002)
+  const port = process.env.PORT || 3000
+  await app.listen(port, '0.0.0.0')
 }
 
 export default bootstrap()
